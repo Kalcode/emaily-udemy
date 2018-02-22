2f42ab3128228eb73bf4957ca197c591e1548de0
David Clausen <davidclausen2051@gmail.com>
2018-02-22 16:36:50 -0600

    connect app to redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 import Header from './Header'
 
@@ -14,21 +16,27 @@ const Landing = () => {
 }
 
 
-const App = () => {
-  return  (
-    <div>
-      <BrowserRouter>
-          <div>
-            <Header />
-            <div className='container'>
-              <Route exact path='/' component={Landing} />
-              <Route exact path='/surveys' component={Dashboard} />
-              <Route path='/surveys/new' component={SurveyNew} />
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+            <div>
+              <Header />
+              <div className='container'>
+                <Route exact path='/' component={Landing} />
+                <Route exact path='/surveys' component={Dashboard} />
+                <Route path='/surveys/new' component={SurveyNew} />
+              </div>
             </div>
-          </div>
-      </BrowserRouter>
-    </div>
-  )
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App
+export default connect(null, actions)(App)
