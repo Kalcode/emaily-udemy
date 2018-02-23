029f19b3489a85d647dea0246948f70cd02947d1
David Clausen <davidclausen2051@gmail.com>
2018-02-23 09:48:35 -0600

    add switch case for login state to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,26 @@
 import React,  { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return <li><a href='/auth/google'>Login With Google</a></li>
+      default:
+        return <li><a href='#'>Logout</a></li>
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className='nav-wrapper'>
           <div className='container'>
             <a href='#' className='brand-logo left'>Logo</a>
-            <ul id='nav-mobile' className='right'>
-              <li><a href='#'>Login With Google</a></li>
+            <ul className='right'>
+              {this.renderContent()}
             </ul>
           </div>
         </div>
@@ -17,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps, null)(Header)
