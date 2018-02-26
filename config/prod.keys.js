c0a397d60b6152b6bd3d7136dba273a4e5973fe6
David Clausen <davidclausen2051@gmail.com>
2018-02-26 14:13:32 -0600

    add new keys for production

--- config/prod.keys.js
+++ config/prod.keys.js
@@ -1,7 +1,15 @@
 module.exports = {
+  // Google API Keys
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+  // DB Connection
   mongoURI: process.env.MONGO_URI,
+
+  // Cookie-Sessions
   cookieKey: process.env.COOKIE_KEY
-  stripe_secret: process.env.STRIPE_SECRET,dsadas
+
+  // Strip API
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY
 }
