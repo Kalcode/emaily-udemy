9a1eb63f2b2aa31675db01c65e13d0fc11f53b1b
David Clausen <davidclausen2051@gmail.com>
2018-03-03 17:58:05 -0600

    add credit to user account upon successful charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,7 @@
 const keys = require('../config/keys')
 const stripe = require('stripe')(keys.stripeSecretKey)
 
+
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
     const charge = await stripe.charges.create({
@@ -9,6 +10,9 @@ module.exports = app => {
       description: '$5 for 5 credits',
       source: req.body.id,
     })
-    console.log(charge);
+    req.user.credits += 5
+    const user = await req.user.save()
+
+    res.send(user)
   })
 }
